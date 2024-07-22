c82892191e21e77d1fc40e009e95fe23f2607b2d
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-07-21 20:31:08 -0700

    creating a intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <>
+        <div>
+            <h1>Welcome to the Career Advisor Interface</h1>
+            <h4>Together, we are going to create your career plan</h4>
+            <h5>Let's start by telling us your name, the career in tech you want to pursue, and any additional information you find relevant to help us build your blueprint for your journey!</h5>
+        </div>
+    </>
+
+
+  )
+}
+
+export default Intro
